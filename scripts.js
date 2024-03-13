df9770103c7a074ae979afbbebd68a4af396d136
MNontokozo <115648971+MNontokozo@users.noreply.github.com>
2024-03-13 08:44:20 +0200

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,5 @@
+import { Dropdown, Collapse, initMDB } from "mdb-ui-kit";
+import { Collapse, Ripple, initMDB } from "mdb-ui-kit";
+
+initMDB({ Collapse, Ripple });
+initMDB({ Dropdown, Collapse });
